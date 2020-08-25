961ea03cf2681b9419f615646cac8e4bf76dc4d0
sanderkk <sander.k.kilen@gmail.com>
2020-08-25 21:45:29 +0200

    Edit GetUsersBusyTimes from Query to Mutation

--- backend/RecAPI/Applications/ApplicationQueries.cs
+++ backend/RecAPI/Applications/ApplicationQueries.cs
@@ -63,37 +63,5 @@ namespace RecAPI.Applications.Queries
             return repository.GetApplicationWithout(applicationsWithInterview ?? new List<string>());
         }
 
-        [Authorize(Policy = "administrator")]
-        public IEnumerable<DateTime> GetApplicationBusyTimes(
-            ApplicationBusyTimesInput input,
-            [Service] IUserRepository userRepository,
-            [Service] IApplicationRepository applicationRepository
-        )
-        {
-            List<DateTime> busyTimes = new List<DateTime>();
-            var application = applicationRepository.GetApplication(input.Application);
-            if (application == null)
-            {
-                ApplicationError.ApplicationExistError();
-            }
-            List<DateTime> applicationBusyTimes = application.Available != null && application.Available.Count() > 0 ? application.Available : new List<DateTime>();
-            busyTimes.AddRange(applicationBusyTimes);
-            foreach(DateTime busyTimePoint in applicationBusyTimes)
-            {
-                DateTime addedDateTime = busyTimePoint;
-                busyTimes.Add(addedDateTime.AddHours(1.0));
-            }
-            List<string> emails = input.InterviewerEmail != null && input.InterviewerEmail.Count() > 0 ? input.InterviewerEmail : new List<string>();
-            var users = userRepository.GetUsersByEmail(emails);
-            foreach(User user in users)
-            {
-                List<DateTime> userBusyTimes = user.BusyTime != null && user.BusyTime.Count() > 0 ? user.BusyTime : new List<DateTime>();
-                List<DateTime> userInterviewTime = user.InterviewTime != null && user.InterviewTime.Count() > 0 ? user.InterviewTime : new List<DateTime>();
-                busyTimes.AddRange(userBusyTimes);
-                busyTimes.AddRange(userInterviewTime);
-            }
-            return busyTimes.Distinct();
-        }
-
     }
 }

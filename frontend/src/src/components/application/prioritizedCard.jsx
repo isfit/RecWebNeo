6864ff318f6e29597a11c845ded4711ff63f87e9
torsteinotterlei <torstein@otterlei.no>
2020-08-31 16:07:37 +0200

    Change edit application function

--- frontend/src/src/components/application/prioritizedCard.jsx
+++ frontend/src/src/components/application/prioritizedCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const PrioritizedCard = ({readOnly, prioritizedValue, setPrioritized}) => {
+const PrioritizedCard = ({ readOnly=false, prioritizedValue, setPrioritized}) => {
 
     return(
         <div className="card mb-2 px-3 py-2 w-100">
             <div className="">
             <p>We understand that it might be hard to prioritize the positons you want to apply for. By unchecking the box below, we know that you don't have any specific preference between the positions you have entered. We can talk about which of the positions suits you best during the interview. </p>
             <div className="flex-grid mb-3">
-                <input type="checkbox" checked={prioritizedValue} onChange={() => setPrioritized(!prioritizedValue)} readOnly={readOnly} />
+                {readOnly ? <input type="checkbox" checked={prioritizedValue} onclick={prioritizedValue} /> : <input type="checkbox" checked={prioritizedValue} onChange={() => setPrioritized(!prioritizedValue)} />}
                 <h6 className="page-title pl-2 mb-0">The positions in my application are prioritized</h6>
             </div>
             </div>

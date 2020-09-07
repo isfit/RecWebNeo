3f39688d87e623f6f30567a75f60c25c214bb12c
Sander Kilen <sander.k.kilen@gmail.com>
2020-09-07 19:08:07 +0200

    Login function

---
+++ backend/LoadData/APIConnector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadData
+{
+    class GraphQLInput
+    {
+        public static string LoginMutation()
+        {
+            string loginMutation = @"
+                    mutation login($email: String!, $password: String!) {
+                      login(email: $email, password: $password)
+                    }
+                ";
+            return loginMutation;
+        }
+    }
+
+
+    class APIConnector
+    {
+
+        private HttpClient client;
+
+        public APIConnector(string baseUri)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(baseUri);
+        }
+
+
+        public async Task<string> Login(string email, string password)
+        {
+            HttpContent content = new StringContent("hello");
+            HttpResponseMessage response = await client.PostAsync(
+                    "/",
+                    content
+                );
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+    }
+}
